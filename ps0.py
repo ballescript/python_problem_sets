91f5422ad03868ca42b84114d39056c28eebb8ce
elballes <33511941+ballescript@users.noreply.github.com>
2019-11-29 13:10:46 -0800

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Problem Set 0
+
+Created on Tue Nov 19 07:08:36 2019
+
+@author: guest1
+"""
+
+import numpy
+
+x = int(input("Enter a number x: "))
+y = int(input("Enter a number y: "))
+print("x**y = ", x**y)
+print("log(x) = ", numpy.log2(x))
